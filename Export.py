2db97adefd1fbb5770a0caaee0c4c43961a6869a
patkap <patkap@users.noreply.github.com>
2018-10-18 16:11:36 +0000

    lint Export.py

--- Export.py
+++ Export.py
@@ -8,28 +8,31 @@ import datetime
 
 def export(dbPath, nameFile):
     con = sqlite3.connect(dbPath)
-    outfile = open("/home/pi/Share/Public/temp.csv",'w')
+    outfile = open("/home/pi/Share/Public/temp.csv", 'w')
     outcsv = csv.writer(outfile)
-    cursor = con.execute('select ID_Reneco , Date, Weight, Note from session where Weight is not null')
-    outcsv.writerow(['Tind_BagueId','DateSaisie','Poids','Notes'])
+    cursor = con.execute(
+        'select ID_Reneco , Date, Weight, Note from session where Weight is not null')  # noqa: E501
+    outcsv.writerow(['Tind_BagueId', 'DateSaisie', 'Poids', 'Notes'])
     outcsv.writerows(cursor.fetchall())
     outfile.close()
     date = datetime.datetime.now()
-    with open("/home/pi/Share/Public/temp.csv",'rt') as csv_in, open("/home/pi/Share/Public/" + nameFile + ".csv",'w') as csv_out:
+    with open("/home/pi/Share/Public/temp.csv", 'r') as csv_in, \
+            open("/home/pi/Share/Public/" + nameFile + ".csv", 'w') as csv_out:
         reader = csv.reader(csv_in)
         writer = csv.writer(csv_out)
-        writer.writerow(['Tind_BagueId','DateSaisie','Poids','Notes'])
+        writer.writerow(['Tind_BagueId', 'DateSaisie', 'Poids', 'Notes'])
         p = 0
         for row in reader:
             if p != 0:
-                writer.writerow([row[0]] + [date.strftime("%d/%m/%Y")] + [row[2]] + [row[3]])
+                writer.writerow(
+                    [row[0]] + [date.strftime("%d/%m/%Y")] + [row[2]] + [row[3]])  # noqa: E501
             else:
                 p = 1
     csv_in.close()
     csv_out.close()
 
-    
-    for csvfile in glob.glob(os.path.join('/home/pi/Share/Public/'+ nameFile+ ".csv")):
+    for csvfile in glob.glob(os.path.join(
+            '/home/pi/Share/Public/', nameFile + ".csv")):
         workbook = Workbook("/home/pi/Share/Public/" + nameFile + ".xlsx")
         worksheet = workbook.add_worksheet()
         with open(csvfile, 'rt') as f:
@@ -40,5 +43,5 @@ def export(dbPath, nameFile):
         workbook.close()
 
 
-if __name__ == '__main__':        
-    export(dbPath)
+# if __name__ == '__main__':
+#     export(dbPath)
