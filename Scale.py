776cc6c6ff68d01c85a244ac663f69dc02699e6d
patkap <patkap@users.noreply.github.com>
2018-10-18 16:23:34 +0000

    lint Scale.py

--- Scale.py
+++ Scale.py
@@ -1,43 +1,38 @@
-#!/usr/bin/python 
-# -*- coding:utf-8 -*-
+#!/usr/bin/env python3
 
-import re 
-import classGetch
-import Screen
+import re
+# import classGetch
+# import Screen
+
 
 def weigh():
     flag = True
     while flag:
         try:
-            weight = raw_input()
+            weight = input()
         except EOFError:
             pass
         if weight:
             flag = False
-            weightInt = int(re.findall("\d+", str(weight)[0]))
+            weightInt = int(re.findall(r"\d+", str(weight)[0]))
             weightString = str(weightInt)
             if len(weightString) >= 0:
                 return weightString
 
+
 def control(weight, data):
-    if  weight < data.Weight_Min_Imp:
-            return "ImpMin"
+    if weight < data.Weight_Min_Imp:
+        return "ImpMin"
     elif weight > data.Weigh_tMax_Imp:
-            return "ImpMax"
+        return "ImpMax"
     elif weight < data.Weight_Min_Path and weight > data.Weigh_tMin_Imp:
-            return "PathMin"
+        return "PathMin"
     elif weight > data.Weight_Max_Path and weight < data.Weight_Max_Imp:
-            return "PathMax"
+        return "PathMax"
     else:
-            return "Normal" 
-
-def validate(result):
-    if result == "Normal":
-        Screen.
+        return "Normal"
 
 
-
-         
-
-
-
+# def validate(result):
+#     if result == "Normal":
+#         Screen.
