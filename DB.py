ea4d3579420c5bbfa81f9c541efbb9c461b98db8
patkap <patkap@users.noreply.github.com>
2018-10-18 14:36:17 +0000

    add .gitignore

--- DB.py
+++ DB.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python 
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-
-
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import *
+from sqlalchemy import (
+    Column, String, DateTime, Integer,
+    create_engine)
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.schema import Index
+# from sqlalchemy.schema import Index
 import datetime
 import glob
 import os
@@ -15,74 +15,84 @@ import Screen
 
 Base = declarative_base()
 
+
 class Session(Base):
     __tablename__ = 'session'
-    ID                      =   Column(Integer, primary_key = True, autoincrement = True)
-    BID                     =   Column(String, index = True)
-    ID_Reneco               =   Column(String, index = True)
-    ID_RFID                 =   Column(String, index = True)
-    Position                =   Column(String)
-    Age                     =   Column(Integer)
-    Date_Last_Weight        =   Column(String)
-    Days_Since_Last_Weight  =   Column(Integer)
-    Last_Weight             =   Column(Integer)
-    Date_Session            =   Column(DateTime)
-    Weight_Min_Path         =   Column(Integer)                                      
-    Weight_Max_Path         =   Column(Integer)
-    Weight_Min_Imp          =   Column(Integer)
-    Weight_Max_Imp          =   Column(Integer)
-    Date                    =   Column(DateTime)
-    Weight                  =   Column(Integer)
-    Note                    =   Column(String)
+    ID = Column(Integer, primary_key=True, autoincrement=True)
+    BID = Column(String, index=True)
+    ID_Reneco = Column(String, index=True)
+    ID_RFID = Column(String, index=True)
+    Position = Column(String)
+    Age = Column(Integer)
+    Date_Last_Weight = Column(String)
+    Days_Since_Last_Weight = Column(Integer)
+    Last_Weight = Column(Integer)
+    Date_Session = Column(DateTime)
+    Weight_Min_Path = Column(Integer)
+    Weight_Max_Path = Column(Integer)
+    Weight_Min_Imp = Column(Integer)
+    Weight_Max_Imp = Column(Integer)
+    Date = Column(DateTime)
+    Weight = Column(Integer)
+    Note = Column(String)
+
     def __repr__(self):
-        return "<Session(ID = '%s', ID_Reneco = '%s', ID_RFID = '%s', Position = '%s', Age = '%s', Date_Last_Weight = '%s', Days_Since_Last_Weight = '%s', Last_Weight = '%s', Date_Session = '%s', Weight_Min_Path = '%s', Weight_Max_Path = '%s', Weight_Min_Imp = '%s', Weight_Max_Imp = '%s', Date = '%s', Weight = '%s', Note = '%s')>" % (
-            self.ID, self.ID_Reneco, self.ID_RFID, self.Position, self.Age, self.Date_Last_Weight, self.Days_Since_Last_Weight, self.Last_Weight, self.Date_Session, self.Weight_Min_Path, self.Weight_Max_Path, self.Weight_Min_Imp, self.Weight_Max_Imp, self.Date, self.Weight, self.Note)
+        return "<Session(ID = '%s', ID_Reneco = '%s', ID_RFID = '%s', Position = '%s', Age = '%s', Date_Last_Weight = '%s', Days_Since_Last_Weight = '%s', Last_Weight = '%s', Date_Session = '%s', Weight_Min_Path = '%s', Weight_Max_Path = '%s', Weight_Min_Imp = '%s', Weight_Max_Imp = '%s', Date = '%s', Weight = '%s', Note = '%s')>" % (  # noqa: E501
+            self.ID, self.ID_Reneco, self.ID_RFID, self.Position, self.Age, self.Date_Last_Weight, self.Days_Since_Last_Weight, self.Last_Weight, self.Date_Session, self.Weight_Min_Path, self.Weight_Max_Path, self.Weight_Min_Imp, self.Weight_Max_Imp, self.Date, self.Weight, self.Note)  # noqa: E501
 
 
 class Log(Base):
     __tablename__ = 'log'
-    ID = Column(Integer, primary_key = True, autoincrement = True)
+    ID = Column(Integer, primary_key=True, autoincrement=True)
     Date = Column(DateTime)
     Comment = Column(String)
+
     def __repr__(self):
-        return "<Log(ID = '%s', Date = '%s', Comment = '%s'>" % (self.ID, self.Date, self.Comment )
-########################################################################################################################################################################################################################################
+        return "<Log(ID = '%s', Date = '%s', Comment = '%s'>" % (
+            self.ID, self.Date, self.Comment)
+
 
 # def dbExists():
 #     strdate = datetime.datetime.now
 
+
 def testFiles():
-    list_of_files = glob.glob('/home/pi/Share/Public/*.db') # * means all if need specific format then *.db
-    if len(list_of_files)== 0:
-        Screen.msg("DATABASE FILE NOT FOUND",'ERROR',True)
+    # * means all if need specific format then *.db
+    list_of_files = glob.glob('/home/pi/Share/Public/*.db')
+    if len(list_of_files) == 0:
+        Screen.msg("DATABASE FILE NOT FOUND", 'ERROR', True)
         exit()
     strdate = datetime.datetime.now().strftime('%Y%m%d')
-    list_of_files = glob.glob('/home/pi/Share/Public/Prep_Weighing_*_'+strdate+'*.db')
-    if len(list_of_files)== 0:
-        Screen.msg("DATABASE OUTDATED",'ERROR',True)
+    list_of_files = glob.glob(
+        '/home/pi/Share/Public/Prep_Weighing_*_' + strdate + '*.db')
+    if len(list_of_files) == 0:
+        Screen.msg("DATABASE OUTDATED", 'ERROR', True)
         exit()
     latest_file = max(list_of_files, key=os.path.getctime)
     return latest_file
+
 
 def get_export_file_name(str_file_name):
     str_splited = str_file_name.split('_')
     species = str_splited[2]
     return species
 
+
 def initDB(dbFile):
-    engine = create_engine('sqlite:///' + dbFile) #Connexion de la base de donnees
-    session = sessionmaker(bind = engine)
+    # Connexion de la base de donnees
+    engine = create_engine('sqlite:///' + dbFile)
+    session = sessionmaker(bind=engine)
     Base.metadata.create_all(engine)
-    
     return session()
+
 
 def testSession():
     db_file = testFiles()
     species = get_export_file_name(db_file)
     strdate = datetime.datetime.now().strftime('%Y%m%d')
-    export_name = 'WeightsFile_' + species + '_' + strdate 
+    export_name = 'WeightsFile_' + species + '_' + strdate
     session = initDB(db_file)
-    date_access = Log(Date = datetime.datetime.now())
+    date_access = Log(Date=datetime.datetime.now())
     session.add(date_access)
     session.commit()
     dateSession = (session.query(Log).filter(Log.ID == 1).first().Date)
@@ -104,7 +114,7 @@ def testBird(data):
             Screen.msg("BIRD NOT IN DATABASE", 'ERROR', True)
             print("4.1")
             t = 0
-            return t 
+            return t
         elif data.Weight is not None: #l'oiseau a deja ete pese
             Screen.error("BIRD ALREADY WEIGHED",'NOTICE',True)
             print("4.2")
@@ -152,4 +162,3 @@ def validateBird( session,result, key, data, weight):
             elif key == "b":
                 flagVal = False
                 Screen.ink(data.ID_Reneco,data.Position,data.Age,weight ,"CANCELLED",  data.Last_Weight, data.Days_Since_Last_Weight)
-
